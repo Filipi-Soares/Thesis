41cb84e0edf51305b295f499ef304ad19dbebf5a
Filipi-Soares <filipisoares@usp.br>
2024-06-17 14:25:12 -0300

    Create PlantSpeciesList.py

---
+++ PlantSpeciesList.py
@@ -0,0 +1,79 @@
+species_names = [483
+2 "Acrocomia aculeata",484
+3 "Allium sativum",485
+4 "Allium schoenoprasum",486
+5 "Anacardium occidentale",487
+6 "Ananas comosus",488
+7 "Annona muricata",489
+8 "Annona squamosa",490
+9 "Arachis hypogaea",491
+10 "Astrocaryum murumuru",492
+11 "Attalea speciosa",493
+12 "Avena byzantina",494
+13 "Avena sativa",495
+14 "Brassica napus",496
+15 "Byrsonima crassifolia",497
+16 "Capsicum",498
+17 "Carapa guianensis",499
+18 "Carica papaya",500
+19 "Caryocar brasiliense",501
+20 "Citrullus lanatus",502
+21 "Citrus aurantiifolia",503
+22 "Citrus limon",504
+23 "Citrus reticulata",505
+24 "Citrus sinensis",506
+25 "Coffea arabica",507
+26 "Coffea canephora",508
+27 "Colocasia esculenta",509
+28 "Coriandrum sativum",510
+29 "Cucumis melo",511
+30 "Cucurbita maxima",512
+31 "Daucus carota",513
+32 "Dimorphandra mollis",514
+33 "Dipteryx odorata",515
+34 "Euterpe edulis", # Duplicate, Euterpe oleracea is the correct name for Acai-516
+verdadeiro517
+35 "Euterpe oleracea",518
+36 "Genipa americana",519
+37 "Glycine max",520
+38 "Gossypium",521
+39 "Hancornia speciosa",522
+40 "Helianthus annuus",523
+41 "Hordeum vulgare",524
+42 "Ipomoea batatas",525
+43 "Lactuca sativa",526
+44 "Malpighia glabra",527
+45 "Malus domestica",528
+13/18
+46 "Mangifera indica",529
+47 "Mauritia flexuosa",530
+48 "Musa acuminata",531
+49 "Musa balbisiana",532
+50 "Oryza glaberrima",533
+51 "Oryza sativa",534
+52 "Passiflora edulis",535
+53 "Pennisetum glaucum",536
+54 "Petroselinum crispum",537
+55 "Phaseolus vulgaris",538
+56 "Piper nigrum",539
+57 "Prunus persica",540
+58 "Psidium guajava",541
+59 "Ricinus communis",542
+60 "Saccharum",543
+61 "Secale cereale",544
+62 "Solanum melongena",545
+63 "Solanum tuberosum",546
+64 "Sorghum bicolor",547
+65 "Spondias mombin",548
+66 "Spondias purpurea",549
+67 "Spondias tuberosa",550
+68 "Syagrus coronata",551
+69 "Tamarindus indica",552
+70 "Theobroma cacao",553
+71 "Theobroma grandiflorum",554
+72 "Triticum x Secale", # Corrected to represent the hybrid nature555
+73 "Triticum",556
+74 "Vicia faba",557
+75 "Vitis vinifera",558
+76 "Zea mays"559
+77 ]
